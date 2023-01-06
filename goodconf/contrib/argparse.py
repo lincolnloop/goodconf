6855e56db891d61f1b46fb76b1dbb56695a17c6f
Florian Apolloner <florian@apolloner.eu>
2023-01-06 11:30:59 +0100

    Added pre-commit configuration for black & isort.

--- goodconf/contrib/argparse.py
+++ goodconf/contrib/argparse.py
@@ -6,9 +6,12 @@ from .. import GoodConf
 def argparser_add_argument(parser: argparse.ArgumentParser, config: GoodConf):
     """Adds argument for config to existing argparser"""
     help = "Config file."
-    if config.__config__.file_env_var:
-        help += f" Can also be configured via the environment variable: {config.__config__.file_env_var}"
-    if config.__config__.default_files:
-        files_str = ", ".join(config.__config__.default_files)
+    cfg = config.__config__
+    if cfg.file_env_var:
+        help += (
+            f"Can also be configured via the environment variable: {cfg.file_env_var}"
+        )
+    if cfg.default_files:
+        files_str = ", ".join(cfg.default_files)
         help += f" Defaults to the first file that exists from [{files_str}]."
     parser.add_argument("-C", "--config", metavar="FILE", help=help)
